d62014caf4f61df50c2025da9a2b9c42eadb39dd
NikolaIstvanic <istvanic.nikola@gmail.com>
2015-09-25 22:32:17 -0400

    First commit of User

---
+++ User.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+
+public class User {
+	
+	private String name;
+	private Calendar calendar;
+	
+	public User(name) {
+		this.name = name;
+		this.calendar = calendar;
+	}
+}
